a028da8e7543df2f655e91aff402ebff3bec95df
Matthew Dwyer <matthewgdwyer@gmail.com>
2022-01-07 16:58:33 +0700

    added scss, finishing for the day

--- src/index.js
+++ src/index.js
@@ -1,7 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '../scss/FileName.scss';
+
+const Header = () => {
+  return (
+    <header>
+      {/*beginning to write react dom*/}
+      <h1 className="title">React Markdown Previewer</h1>
+    </header>
+  );
+};
+const Editor = () => {
+  return (
+  <div className="editor">
+      <h1>Editor</h1>
+  </div>
+    );
+}
+const Preview = () => {
+  return (
+  <div className="preview">
+    <h1>Preview</h1>
+  </div>
+    );
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Editor />
+        <Preview />
+      </div>
+    )
+  }
+};
 
 ReactDOM.render(
-    <h1>Hello World</h1>,
+    <App />,
   document.getElementById('app')
 );

ef5fd7ed1c1a0f973111224490db5c4ab34e37f5
Matthew Dwyer <matthewgdwyer@gmail.com>
2022-01-13 17:00:48 +0700

    server corrected

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get("/", function(req, res) {
 );
 
 app.use(express.static(__dirname + '/dist'));
-app.use(express.static("/dist", `${__dirname}/public`));
+app.use("/dist", express.static(`${__dirname}~~/dist`));
 
 
 

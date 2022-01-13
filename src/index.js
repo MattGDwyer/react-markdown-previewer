764a4ef0f3ad753d42ea3eba38c6dd7f3fdb8dce
Matthew Dwyer <matthewgdwyer@gmail.com>
2022-01-13 15:02:03 +0700

    markdown rendering properly, adjusting rendered markdown layout

--- src/index.js
+++ src/index.js
@@ -4,7 +4,101 @@ import '../dist/styles/main.scss';
 import fCCLogo from '../src/assets/free-code-camp-brands.svg';
 import githubLogo from '../src/assets/Github-Mark-32px.png';
 import linkedinLogo from '../src/assets/linkedin-brands.svg';
+import { marked } from 'marked';
 
+marked.setOptions({
+  breaks: true
+});
+
+const renderer = new marked.Renderer();
+
+const Header = () => {
+  return (
+    <header>
+      {/*beginning to write react dom*/}
+      <span><em><a href="https://www.freecodecamp.org/fcc315d258e-2a09-4d37-8211-d6355054806e" target="_blank"><img src={fCCLogo} alt="free code camp logo" /></a></em></span>
+      <h1 className="title">React Markdown Previewer</h1>
+      <nav className="nav">
+        <ul>
+          <li><a href="https://github.com/MattGDwyer" target="_blank"><img src={githubLogo} alt="github logo" /></a></li>
+          <li><a href="https://www.linkedin.com/in/matthew-dwyer-5289a119b/" target="_blank"><img src={linkedinLogo} alt="linked in logo"/></a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+const Editor = (props) => {
+  return (
+    <div className="editor">
+      <div className="sub-header">
+      <h2>Editor</h2>
+      </div>
+      <textarea
+      id="editor"
+      onChange={props.onChange}
+      type="text"
+      value={props.markdown}
+      />
+  </div>
+    );
+  }
+  const Preview = (props) => {
+    return (
+      <div className="preview">
+        <div className="sub-header">
+        <h2>Preview</h2>
+        </div>
+        <div
+        dangerouslySetInnerHTML={{
+          __html: marked(props.markdown, { renderer: renderer })
+        }}
+        id="preview"></div>
+  </div>
+    );
+  }
+  const Footer = () => {
+    return (
+      <footer>
+      <div className="my-name"><h3>Matthew Dwyer</h3></div>
+            <nav className="nav">
+        <ul>
+          <li><a href="https://github.com/MattGDwyer" target="_blank"><img src={githubLogo} alt="github logo" /></a></li>
+          <li><a href="https://www.linkedin.com/in/matthew-dwyer-5289a119b/" target="_blank"><img src={linkedinLogo} alt="linked in logo"/></a></li>
+          <li><a href="https://www.freecodecamp.org/fcc315d258e-2a09-4d37-8211-d6355054806e" target="_blank"><img src={fCCLogo} alt="free code camp logo" /></a></li>
+        </ul>
+      </nav>
+    </footer>
+  )
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      markdown: placeHolder
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    this.setState({
+      markdown: event.target.value
+    });
+  }
+
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <div className="app-box">
+          <Editor markdown={this.state.markdown} onChange={this.handleChange}/>
+          <Preview markdown={this.state.markdown}/>
+        </div>
+        <Footer />
+      </div>
+    )
+  }
+};
 
 const placeHolder = `# Welcome to my React Markdown Previewer!
 
@@ -51,76 +145,7 @@ And here. | Okay. | I think we get it.
 ![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
 `;
 
-const Header = () => {
-  return (
-    <header>
-      {/*beginning to write react dom*/}
-      <span><em><a href="https://www.freecodecamp.org/fcc315d258e-2a09-4d37-8211-d6355054806e" target="_blank"><img src={fCCLogo} alt="free code camp logo" /></a></em></span>
-      <h1 className="title">React Markdown Previewer</h1>
-      <nav className="nav">
-        <ul>
-          <li><a href="https://github.com/MattGDwyer" target="_blank"><img src={githubLogo} alt="github logo" /></a></li>
-          <li><a href="https://www.linkedin.com/in/matthew-dwyer-5289a119b/" target="_blank"><img src={linkedinLogo} alt="linked in logo"/></a></li>
-        </ul>
-      </nav>
-    </header>
-  );
-};
-const Editor = () => {
-  return (
-  <div className="editor">
-      <div className="sub-header">
-      <h2>Editor</h2>
-      </div>
-      <textarea id="editor" value={placeHolder}></textarea>
-  </div>
-    );
-}
-const Preview = () => {
-  return (
-  <div className="preview">
-    <div className="sub-header">
-    <h2>Preview</h2>
-    </div>
-    <div id="preview"></div>
-  </div>
-    );
-}
-const Footer = () => {
-  return (
-    <footer>
-      <div className="my-name"><h3>Matthew Dwyer</h3></div>
-            <nav className="nav">
-        <ul>
-          <li><a href="https://github.com/MattGDwyer" target="_blank"><img src={githubLogo} alt="github logo" /></a></li>
-          <li><a href="https://www.linkedin.com/in/matthew-dwyer-5289a119b/" target="_blank"><img src={linkedinLogo} alt="linked in logo"/></a></li>
-          <li><a href="https://www.freecodecamp.org/fcc315d258e-2a09-4d37-8211-d6355054806e" target="_blank"><img src={fCCLogo} alt="free code camp logo" /></a></li>
-        </ul>
-      </nav>
-    </footer>
-  )
-}
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div>
-        <Header />
-        <div className="app-box">
-          <Editor />
-          <Preview />
-        </div>
-        <Footer />
-      </div>
-    )
-  }
-};
-
 ReactDOM.render(
-    <App />,
+  <App />,
   document.getElementById('app')
 );

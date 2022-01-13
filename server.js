cfc60a22e85a526608e12301ac89c7db96346f30
Matthew Dwyer <matthewgdwyer@gmail.com>
2022-01-13 16:51:20 +0700

    revised server.js

--- server.js
+++ server.js
@@ -2,16 +2,17 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// app.get("/", function(req, res) {
-//   res.sendFile(`${__dirname}../client/src/index.js`);
-//   }
-// );
+app.get("/", function(req, res) {
+  res.sendFile(`${__dirname}/dist/index.html`);
+  }
+);
 
-// app.use(express.static(`${__dirname}/public`));
-app.use(express.static(__dirname + 'dist'));
+app.use(express.static(__dirname + '/dist'));
+app.use(express.static("/dist", `${__dirname}/public`));
 
 
 
 app.listen(PORT, () => {
     console.log(`Server is running on PORT:' ${PORT}`);
 });
+
